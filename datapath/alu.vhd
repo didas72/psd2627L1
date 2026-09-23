193b7eb6329d9b3729414c6b44e47ecb6d70be12
Rodrigo Caldeira <rodishumy@gmail.com>
2026-09-23 23:06:52 +0100

    UPDATE: Finished sum operation (i hope) on the ALU

--- datapath/alu.vhd
+++ datapath/alu.vhd
@@ -18,7 +18,13 @@ architecture archi of alu is
 begin
     -- SUM
     res_sum <= OPERAND1 + OPERAND2;
-    sum_ofl <=
+    if OPERAND1(OPERAND1'LEFT) xor OPERAND2(OPERAND2'LEFT) then -- if the operand's signs are different, no overflow can occur
+        sum_ofl <= 0;
+    elsif RESULT(RESULT'LEFT) xor OPERAND1(OPERAND1'LEFT) then -- if signs are the same but the result's sign is different
+        sum_ofl <= 1; 
+    else
+        sum_ofl <= 0;   -- signs are the same and the result's sign is also the same
+    end if;
     -- MUL
     res_mul <= OPERAND1 * OPERAND2;
     mul_ofl <=
@@ -29,6 +35,31 @@ begin
     res_shift <= sign_bit & OPERAND1(OPERAND1'LEFT - 1 downto 1); -- "OPERAND1'LEFT - 1" = Second MSB
 
     -- Set RESULT according to chosen operation
+    case OPER is
+        when 0 =>
+            RESULT <= res_sum(N-1 downto 0);
+            ofl <= sum_ofl;
+        when 1 =>
+            RESULT <= res_mul;
+            ofl <= mul_ofl;
+        when 2 =>
+            RESULT <= res_or;
+            ofl = 0;
+        when others =>
+            RESULT <= res_shift;
+            ofl = 0;
+
     
+    --with OPER select
+    --    RESULT <= res_sum when 0,
+    --              res_mul when 1,
+    --              res_or when 2,
+    --              res_shift when others;
+    
+    --with OPER select     
+    --    ofl <= sum_ofl when 0,
+    --           mul_ofl when 1,
+    --           0 when others;
+
     
 end archi;
